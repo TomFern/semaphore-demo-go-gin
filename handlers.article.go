17b8ce28d52e0802700612c7990a95cc186e7f31
tomas f <46322567+TomFern@users.noreply.github.com>
2022-05-12 13:13:06 -0300

    update render function

--- handlers.article.go
+++ handlers.article.go
@@ -38,3 +38,24 @@ func getArticle(c *gin.Context) {
     c.AbortWithStatus(http.StatusNotFound)
   }
 }
+
+
+
+// Render one of HTML, JSON or CSV based on the 'Accept' header of the request
+// If the header doesn't specify this, HTML is rendered, provided that
+// the template name is present
+func render(c *gin.Context, data gin.H, templateName string) {
+
+  switch c.Request.Header.Get("Accept") {
+  case "application/json":
+    // Respond with JSON
+    c.JSON(http.StatusOK, data["payload"])
+  case "application/xml":
+    // Respond with XML
+    c.XML(http.StatusOK, data["payload"])
+  default:
+    // Respond with HTML
+    c.HTML(http.StatusOK, templateName, data)
+  }
+
+}
--- main.go
+++ main.go
@@ -3,8 +3,6 @@
 package main
 
 import (
-  "net/http"
-
   "github.com/gin-gonic/gin"
 )
 
@@ -23,22 +21,3 @@ func main() {
     router.Run()
 }
 
-
-// Render one of HTML, JSON or CSV based on the 'Accept' header of the request
-// If the header doesn't specify this, HTML is rendered, provided that
-// the template name is present
-func render(c *gin.Context, data gin.H, templateName string) {
-
-  switch c.Request.Header.Get("Accept") {
-  case "application/json":
-    // Respond with JSON
-    c.JSON(http.StatusOK, data["payload"])
-  case "application/xml":
-    // Respond with XML
-    c.XML(http.StatusOK, data["payload"])
-  default:
-    // Respond with HTML
-    c.HTML(http.StatusOK, templateName, data)
-  }
-
-}
